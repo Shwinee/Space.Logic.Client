64a9ec48d0e3cbcf30bbb7a9dfc5feb3db28f153
Shwinee <justecca2003@gmail.com>
2020-12-03 22:40:13 -0600

    0.0.2.6

--- sendRecive.js
+++ sendRecive.js
@@ -1,14 +1,20 @@
 const fetch = require('node-fetch');
 
 async function getdata(){
-  const response = await fetch('https://poggame.herokuapp.com/api');
+  const response = await fetch('http://localhost:3000/api');
   const data = await response.json();
 
   for (item of data ){
-    if(item.name !== ""){
-      console.log(item.name);
+    if(item.info.name !== ""){
+      console.log(`${item.info.name}-${item.info.job}`);
     }
   }
 }
+async function getgamedata(){
+  const response = await fetch('http://localhost:3000/game');
+  const data = await response.json();
 
-module.exports = { getdata };
+  console.log();
+}
+
+module.exports = { getdata, getgamedata };
